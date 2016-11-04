f5945730cf7eddecac8b8e8589088f55a78cd408
mac <mac@macdeiMac.local>
2016-11-04 17:48:56 +0800

    index finish

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,46 @@
-$('#fullpage').fullpage({
-	//section背景颜色
-	//sectionsColor:['green','orange','gray','red'],
-	//是否使用箭头控释slide幻灯片
-	controlArrows:true,
-	//每一页的内容是否垂直居中
-	verticalCentered:false,
-	//字体是否随着窗口缩放而缩放
-	resieze:true,
-
-	//滚动速度，单位为毫秒，默认为700
-	scrollingSpeed:800,
-	//定义锚连接，默认为空，可以快速定位到某一个页面
-	anchors:['page1','page2','page3','page4','page5','page6','page7','page8'],
-	//是否使用CSS3属性来实现滚动效果
-	css3:true,
-
-	//滚动到顶部后是否滚到底部
-	loopTop:false,
-	//滚动到底部后是否滚到顶部
-	loopBottom:true,
-	//横向slider幻灯片是否循环滚动
-	loopHorizontal:true,
-	//固定的元素，页面滚动的时候 元素固定不东
-	//fixedElements:'#header',
-	// paddingTop:'122px',
-	// paddingBottom:'44px',
-	//绑定菜单
-	//menu:'#header',
-
-	navigation:true,
-	navigationPosition:'right',
-	afterLoad:function(anchorLink,index){
-		console.log('进入的页面为'+anchorLink, index);
-		$('.section').eq(index).find('.items').removeClass('active');
-	},
-	onLeave:function(index,nextIndex,direction){
-		console.log(index, nextIndex, direction);
-
-		$('.section').eq(index).find('.items').toggleClass('active');
-	}
-});
-$.fn.fullpage.destroy();
+// $('#fullpage').fullpage({
+// 	//section背景颜色
+// 	//sectionsColor:['green','orange','gray','red'],
+// 	//是否使用箭头控释slide幻灯片
+// 	controlArrows:true,
+// 	//每一页的内容是否垂直居中
+// 	verticalCentered:false,
+// 	//字体是否随着窗口缩放而缩放
+// 	resieze:true,
+//
+// 	//滚动速度，单位为毫秒，默认为700
+// 	scrollingSpeed:800,
+// 	//定义锚连接，默认为空，可以快速定位到某一个页面
+// 	anchors:['page1','page2','page3','page4','page5','page6','page7','page8'],
+// 	//是否使用CSS3属性来实现滚动效果
+// 	css3:true,
+//
+// 	//滚动到顶部后是否滚到底部
+// 	loopTop:false,
+// 	//滚动到底部后是否滚到顶部
+// 	loopBottom:true,
+// 	//横向slider幻灯片是否循环滚动
+// 	loopHorizontal:true,
+// 	//固定的元素，页面滚动的时候 元素固定不东
+// 	//fixedElements:'#header',
+// 	// paddingTop:'122px',
+// 	// paddingBottom:'44px',
+// 	//绑定菜单
+// 	//menu:'#header',
+//
+// 	navigation:true,
+// 	navigationPosition:'right',
+// 	afterLoad:function(anchorLink,index){
+// 		console.log('进入的页面为'+anchorLink, index);
+// 		$('.section').eq(index).find('.items').removeClass('active');
+// 	},
+// 	onLeave:function(index,nextIndex,direction){
+// 		console.log(index, nextIndex, direction);
+//
+// 		$('.section').eq(index).find('.items').toggleClass('active');
+// 	}
+// });
+// $.fn.fullpage.destroy('all');
 var a=$('.huashiwenp').html();
 var str='';
 for(var i in a){
@@ -59,7 +59,6 @@ function items(par,tag,animName){
 	var num=Math.floor(Math.random()*animates.length);
 	par.delegate(tag,'mouseover',function(even){
 
-		console.log(num)
 		if($(this).hasClass('animated')){
 			$(this).addClass(animates[num])
 		}
@@ -109,7 +108,71 @@ $('.trade').delegate('div.joy','mouseout',function(){
 })(jQuery,window,document));
 $('#fullpage').scrollactive();
 
-
+;(function($,win,doc,underfine){
+	var menth={
+		init:function(self,opt){
+			var warp=$(self).find('.car-warp');
+			var view=$(self).find('.car-view');
+			view.find('li.car-img').eq(0).fadeIn()
+			var control=$(self).find('.car-control');
+			menth.controlInit(control,view,opt);
+			if(opt.auto){
+				menth.autoplay(view,control,opt)
+			}
+		},
+		imgSize:function(view){
+			return $(view).find('.car-img').length;
+		},
+		autoplay:function(view,control,opt){
+			var i=opt.active;
+			var corl=control.find('li');
+			var viewItem=view.find('.car-img');
+			var size=viewItem.length;
+			setInterval(function(){
+				if(i<size){
+					viewItem.fadeOut();
+					viewItem.eq(i).fadeIn();
+					corl.removeClass('active');
+					corl.eq(i).addClass('active');
+					i++;
+				}else {
+					i=0;
+				}
+			},opt.time)
+		},
+		controlInit:function(control,view){
+			var size=menth.imgSize(view);
+			var str='';
+			for(var i=0;i<size;i++){
+				str+='<li>'+(i+1)+'</li>'
+			};
+			control.html(str);
+			control.find('li').eq(0).addClass('active');
+			menth.hover(control,view);
+		},
+		hover:function(control,view,opt){
+			control.delegate('li','mouseover',function(){
+				$(view).find('li.car-img').css('zIndex',8).fadeOut(300);
+				$(control).find('li').removeClass('active');
+				$(this).addClass('active');
+				$(view).find('li.car-img').eq($(this).index()).css('zIndex',9).fadeIn(300);
+				opt.active=$(this).index();
+			})
+		}
+	}
+	$.fn.caro=function(options){
+		var def={
+			active:0,
+			time:4000
+		};
+		this.settings=$.extend({},def,options);
+		return menth.init(this,this.settings);
+	}
+}(jQuery,window,document));
+$('.student-carusel').caro({
+	auto:true,
+	time:2000
+});
 
 
 
