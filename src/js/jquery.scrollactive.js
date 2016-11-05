1992bd76315cde02e3133faa6156c0d29e6e748d
mac <mac@macdeiMac.local>
2016-11-05 16:03:02 +0800

    ....

--- src/js/jquery.scrollactive.js
+++ src/js/jquery.scrollactive.js
@@ -8,25 +8,92 @@
 		},
 		init:function(self,opt){
 			var items=self.find('.items');
+			$(items).attr('anim',false);
 			var browers=meath.browserV();
-			meath.isltIE(items,browers)
+			if(meath.isltIE(browers)){
+				meath.IEinit(items,opt);
+
+				meath.IEanimation(items,opt);
+			}else {
+				$(win).scroll(function(){
+					$(items).each(function(i,ele){
+						if(meath.isSee($(ele))&&!$(ele).hasClass('active')){
+							$(ele).addClass('active');
+						}
+					})
+				});
+			}
+
+		},
+		IEanimation:function(items,opt){
+			var len=items.length;
 			$(win).scroll(function(){
-				$(items).each(function(i,ele){
-					if(meath.isSee($(ele))&&!$(ele).hasClass('active')){
-						$(ele).addClass('active');
+				for(var i=0;i<len;i++){
+					var ele=items[i];
+					if(meath.isSee($(ele))){
+						var css=meath.dirpanduan(ele,opt,'end');
+						if(css!==underfine){
+							console.log(!!$(ele).attr('anim'));
+							if(!!$(ele).attr('anim')){
+								meath.animate(ele,css)
+							}
+						}
 					}
-				})
+				}
 			});
 		},
-		isltIE:function(items,browers){
+		animate:function(ele,css){
+			$(ele).animate(css,700);
+			$(ele).attr('anim',true)
+		},
+		//判断是ie10以下版本
+		isltIE:function(browers){
 			var a=parseInt(browers[browers.length-1]);
-			console.log(a,browers[browers.length-1]);
 			if(isNaN(a)){
-				console.log('这个不是ie的浏览器');
-			}else if(a<10){
-				console.log('这个是ie浏览器 并且低于ie10');
+				alert('这不是ie')
+				return false;
+			}else if(a<=10){
+				alert('这是ie')
+				return true;
 			}
 		},
+		//如果是ie10那么进行样式初始化
+		IEinit:function(items,opt){
+			var len=items.length;
+			for(var i=0;i<len;i++){
+				var item=items[i];
+				var css=meath.dirpanduan(item,opt,'start');
+				if(css!==underfine){
+					$(item).css(css);
+				}
+			}
+		},
+		//动画方向判断
+		dirpanduan:function(item,opt,type){
+			var opt=opt.css;
+			if(type=='start'){
+				if($(item).hasClass('items-left')){
+					return opt.startleft
+				}else if($(item).hasClass('items-right')){
+					return opt.startright
+				}else if($(item).hasClass('items-top')){
+					return opt.starttop
+				}else if($(item).hasClass('items-bottom')){
+					return opt.startbottom
+				}
+			}else if(type=='end'){
+				if($(item).hasClass('items-left')){
+					return opt.endleft
+				}else if($(item).hasClass('items-right')){
+					return opt.endright
+				}else if($(item).hasClass('items-top')){
+					return opt.endtop
+				}else if($(item).hasClass('items-bottom')){
+					return opt.endbottom
+				}
+			}
+		},
+		//获取浏览器内核
 		browserV:function(){
 			var browser=win.navigator.userAgent
 			var isOpera=browser.indexOf('Opera')>-1;
@@ -62,6 +129,17 @@
 	}
 	$.fn.scrollactive=function(options){
 		var def={
+			css:{
+					startleft:{marginLeft:'-1000px'},
+					endleft:{marginLeft:0,opacity:1},
+					starttop:{mariginTop:'-300px'},
+					endtop:{mariginTop:0,opacity:1},
+					startbottom:{marginBottom:'300px'},
+					endbottom:{marginBottom:0,opacity:1},
+					startright:{marginRight:'1000px'},
+					endright:{marginRight:0,opacity:1},
+
+				}
 		};
 		this.settings=$.extend(def,options);
 		return meath.init(this,this.settings);
